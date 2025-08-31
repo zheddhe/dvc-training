e21c354a1bdb9874ae05204a7a42342a3647e54e
Remy <remy.canal@live.fr>
2025-08-31 08:52:00 +0000

    Ajout dag/repro et changement stratégie échapement guillemets

--- dvc_oauth2_set.sh
+++ dvc_oauth2_set.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dvc remote modify \
 	${1:-remote_gdrive} \
-	gdrive_client_id "${2:-gdrive_client_id}"
+	gdrive_client_id ${2:-gdrive_client_id}
 dvc remote modify \
 	${1:-remote_gdrive} \
-	gdrive_client_secret "${3:-gdrive_client_secret}"
+	gdrive_client_secret ${3:-gdrive_client_secret}
